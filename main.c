bff5b2c6f5320b2750bdc06be38700b6c60aad7f
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2015-11-04 16:49:43 +0800

    Reverted back initialising testing for debugging

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <omp.h>
 #include <sys/time.h>
 
-#define MAT_SIZE 100000000
-#define NUM_THREADS 4
+#define MAT_SIZE 10
+#define NUM_THREADS 2
 #define WORK_SIZE (MAT_SIZE / NUM_THREADS)
 #define REDUCED_SIZE (2 * NUM_THREADS - 2)
 
@@ -103,14 +103,14 @@ void solve(
 }
 
 int main(void) {
-    // const double a[] = {0.0,9.56926,9.96821,9.22169,2.86898,6.25015,1.40662,4.10846,8.39917,5.04963};
-    // const double b[] = {9.91092,8.11347,1.99982,9.07977,3.11674,5.30049,2.55666,3.01966,5.73383,2.05164};
-    // const double c[] = {4.17284,5.81033,5.52629,6.55847,2.22006,6.39581,1.12284,4.85695,3.41811,0.0};
-    // const double r[] = {8.43678, 4.59904, 7.20084, 1.53088, 7.56609, 2.8395, 5.57885, 4.17973, 7.71119, 7.35723};
-    double* a = calloc(MAT_SIZE, sizeof(double));
-    double* b = calloc(MAT_SIZE, sizeof(double));
-    double* c = calloc(MAT_SIZE, sizeof(double));
-    double* r = calloc(MAT_SIZE, sizeof(double));
+    const double a[] = {0.0,9.56926,9.96821,9.22169,2.86898,6.25015,1.40662,4.10846,8.39917,5.04963};
+    const double b[] = {9.91092,8.11347,1.99982,9.07977,3.11674,5.30049,2.55666,3.01966,5.73383,2.05164};
+    const double c[] = {4.17284,5.81033,5.52629,6.55847,2.22006,6.39581,1.12284,4.85695,3.41811,0.0};
+    const double r[] = {8.43678, 4.59904, 7.20084, 1.53088, 7.56609, 2.8395, 5.57885, 4.17973, 7.71119, 7.35723};
+    // double* a = calloc(MAT_SIZE, sizeof(double));
+    // double* b = calloc(MAT_SIZE, sizeof(double));
+    // double* c = calloc(MAT_SIZE, sizeof(double));
+    // double* r = calloc(MAT_SIZE, sizeof(double));
     double* s = calloc(MAT_SIZE, sizeof(double));
 
     double* reducedA = malloc(REDUCED_SIZE * sizeof(double));
@@ -118,16 +118,16 @@ int main(void) {
     double* reducedC = malloc(REDUCED_SIZE * sizeof(double));
     double* reducedR = malloc(REDUCED_SIZE * sizeof(double));
 
-    srand(time(NULL));
-    for (int i = 0; i < MAT_SIZE; ++i) {
-        a[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
-        b[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
-        c[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
-        r[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
-    }
-
-    a[0] = 0.0;
-    c[MAT_SIZE-1] = 0.0;
+    // srand(time(NULL));
+    // for (int i = 0; i < MAT_SIZE; ++i) {
+    //     a[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
+    //     b[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
+    //     c[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
+    //     r[i] = 1.0 + (double) rand() / (RAND_MAX) * 9.0;
+    // }
+    //
+    // a[0] = 0.0;
+    // c[MAT_SIZE-1] = 0.0;
 
     struct timeval start, diff;
     gettimeofday(&start, NULL);
@@ -155,12 +155,10 @@ int main(void) {
     gettimeofday(&start, NULL);
     double* other_solution = tridiagonal_solve(a, b, c, r, MAT_SIZE);
     gettimeofday(&diff, NULL);
-    printf("Linear takes %f\n", ((diff.tv_sec - start.tv_sec) * 1e6 + (diff.tv_usec - start.tv_usec))/1e6);
+    printf("Linear   takes %f\n", ((diff.tv_sec - start.tv_sec) * 1e6 + (diff.tv_usec - start.tv_usec))/1e6);
 
-    other_solution++;
-
-    /*printf("Solution is \n");
+    printf("Solution is \n");
     for (int i = 0; i < MAT_SIZE; ++i) {
         printf("%d: %f %f\n", i, s[i], other_solution[i]);
-    }*/
+    }
 }
